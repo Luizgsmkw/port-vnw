266a0b6b841aa2c3b7741be5b3cdadea73f1de91
Luizgsmkw <luizgustavokw@gmail.com>
2022-10-06 18:31:12 -0300

    finalização da home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import * as S from "../styles/mainStyled";
+import FotoPerfil from "../img/fotoPerfil.png";
+import curriculo from "../../assets/curriculo/Luiz-Martins.pdf";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+      <S.Section>
+        <S.DivInfos>
+          <S.P>
+            <span>Oi, eu sou</span>
+            <S.SpanName>Luiz Martins.</S.SpanName>
+            <span>Desenvolvedor Front-end</span>
+          </S.P>
+          <S.Btn
+            href={curriculo}
+            download="Luiz-Martins.pdf"
+            type="application/pdf"
+          >
+            Download Currículo
+          </S.Btn>
+        </S.DivInfos>
+       
+          <S.FotoPerfil src={FotoPerfil} alt="Foto de perfil" />
+      
+      </S.Section>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/components/styles/mainStyled.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+
+export const Section = styled.section `
+    height: 89.8vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    @media(max-width: 650px) {
+        flex-direction: column;
+        justify-content: center;
+    }
+`
+export const DivInfos = styled.div `
+    @media(max-width: 650px) {
+        order: 1;
+    }
+
+`
+export const P = styled.p `
+    display: flex;
+    flex-direction: column;
+    font-size: 55px;
+    color: #F2F2F2;
+    font-weight: 500;
+    margin-bottom: 40px;
+
+     span{
+        margin-bottom: 10px;
+     }
+
+     @media(max-width: 1080px) {
+        font-size: 40px;
+    }
+
+    @media(max-width: 770px) {
+        font-size: 30px;
+    }
+`
+
+export const SpanName = styled.span `
+    color: #6CDFE1;
+    font-weight: 700;
+`
+
+export const Btn = styled.a `
+    background-image: linear-gradient(to right, #0FC2C0, #0CABA8, #008F8C);
+    padding: 15px 30px;
+    border-radius: 30px;
+    text-decoration: none;
+    color: #F2F2F2;
+`
+
+export const FotoPerfil = styled.img `
+    @media(max-width: 1080px) {
+        width: 300px;
+    }
+
+    @media(max-width: 650px) {
+        order: 0;
+        margin-bottom: 40px;
+    }
+
+`
